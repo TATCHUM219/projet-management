dd7bb2a84cbf78af184d24a627f40ee1465b77c0
TATCHUM219 <tatchumkamgajordandouglas@gmail.com>
2025-07-21 11:11:06 +0100

    cleanning for deploy14

--- app/api/message/[id]/read/route.ts
+++ app/api/message/[id]/read/route.ts
@@ -3,8 +3,8 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
-  const messageId = params.id;
+export async function PUT(req: NextRequest, context: { params: Record<string, string> }) {
+  const messageId = context.params.id;
   try {
     const message = await prisma.message.update({
       where: { id: messageId },
