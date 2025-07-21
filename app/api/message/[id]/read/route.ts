31da4b49336c2bb1e58c850b7a9bdd1ebc21200c
TATCHUM219 <tatchumkamgajordandouglas@gmail.com>
2025-07-21 11:52:30 +0100

    cleanning for deploy18

--- app/api/message/[id]/read/route.ts
+++ app/api/message/[id]/read/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
-  const messageId = params.id;
   // traite la mise à jour du message ici
   return NextResponse.json({ success: true });
 } 
